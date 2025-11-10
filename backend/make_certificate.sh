b6d5428b9ec2a1ff312287e642e2507c86c05707
Alexander Anderson <alexander@wlan802-1837.resnet.cwu.edu>
2025-11-10 09:04:54 -0800

    Client rework

--- backend/make_certificate.sh
+++ backend/make_certificate.sh
@@ -4,7 +4,16 @@ echo "Generating self-signed certificate for secure https connection"
 
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 9999
 
-#mv serverkeystore.jks ./src/main/resources/.
+#the password for the truststore is password 😎
+keytool -importcert \
+    -file cert.pem \
+    -alias serverCert \
+    -keystore truststore.p12 \
+    -storetype PKCS12 \
+    -storepass password
+
+#key.pem must stay in the backend!
 mv key.pem ./src/main/resources/.
-cp cert.pem ./src/main/resources/.
-mv cert.pem ../frontend/src/main/resources/.
+mv cert.pem ./src/main/resources/.
+#public key anyone can use
+mv truststore.p12 ../frontend/src/main/resources/.
--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.poke_frontend {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
     exports com.poke_frontend;
     opens com.poke_frontend to javafx.fxml;
 }
