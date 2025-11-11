103054f974bffed3c38013e39f9feb938190057d
Alan Ruelas-Cordova <ruelascordA@cwu.edu>
2025-11-11 10:06:14 -0800

    added menuPage.java and mainMenu.fxml files

---
+++ frontend/src/main/java/com/poke_frontend/MainPage.java
@@ -0,0 +1,49 @@
+package com.poke_frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainPage {
+
+    @FXML
+    private Button databaseButton_Main;
+
+    @FXML
+    private Button exitButton_Menu;
+
+    @FXML
+    private Button inventoryButton_Main;
+
+    @FXML
+    private Button logOutButton_Menu;
+
+    @FXML
+    private Button shopButton_Main;
+
+    @FXML
+    void closeApplication(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToDatabase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToInventory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToShop(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+}
