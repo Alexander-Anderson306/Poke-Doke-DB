233cdd9dd5b51e0d4bb66b331ded1cf54038e940
Alan Ruelas-Cordova <ruelascordA@cwu.edu>
2025-11-11 19:25:43 -0800

    Created the ViewPage.java and viewCards.fxml

--- frontend/src/main/java/com/poke_frontend/App.java
+++ frontend/src/main/java/com/poke_frontend/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(FXMLLoader.load(App.class.getResource("/fxml/mainMenu.fxml")));
+        scene = new Scene(FXMLLoader.load(App.class.getResource("/fxml/viewCards.fxml")));
         stage.setScene(scene);
         stage.show();
     }
---
+++ frontend/src/main/java/com/poke_frontend/ViewPage.java
@@ -0,0 +1,102 @@
+package com.poke_frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ViewPage {
+
+    @FXML
+    private Button accountButton_view;
+
+    @FXML
+    private Button buyButton_View;
+
+    @FXML
+    private AnchorPane cardView;
+
+    @FXML
+    private Button homeButton_View;
+
+    @FXML
+    private ImageView imageSlot;
+
+    @FXML
+    private ImageView imageSlot1;
+
+    @FXML
+    private ImageView imageSlot11;
+
+    @FXML
+    private ImageView imageSlot111;
+
+    @FXML
+    private ImageView imageSlot1111;
+
+    @FXML
+    private ImageView imageSlot11111;
+
+    @FXML
+    private ImageView imageSlot112;
+
+    @FXML
+    private ImageView imageSlot1121;
+
+    @FXML
+    private ImageView imageSlot12;
+
+    @FXML
+    private ImageView imageSlot121;
+
+    @FXML
+    private ImageView imageSlot2;
+
+    @FXML
+    private ImageView imageSlot21;
+
+    @FXML
+    private ScrollPane scrollView;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button searchButton_View;
+
+    @FXML
+    private Button sortButton_View;
+
+    @FXML
+    private Label username_label;
+
+    @FXML
+    void goToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToMainMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToShop(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchCards(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sortCards(ActionEvent event) {
+
+    }
+
+}
