034a083183f2fac3430c1a9e62794fd4f9146ed2
Eduardo <garcilazose@cwu.edu>
2025-11-17 22:08:41 -0800

    Add SQLHandler method signatures

--- backend/src/main/java/com/poke_backend/SQLHandler.java
+++ backend/src/main/java/com/poke_backend/SQLHandler.java
@@ -3,7 +3,86 @@ package com.poke_backend;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
+
+import com.poke_backend.User;
+import com.poke_backend.Card;
+import com.poke_backend.CardPack;
+import com.poke_backend.UserInventory;
+
+import com.poke_backend.CreateAccountRequest;
+import com.poke_backend.LoginRequest;
+import com.poke_backend.LogoutRequest;
+import com.poke_backend.InventoryRequest;
+import com.poke_backend.AllCardsRequest;
+import com.poke_backend.PackRequest;
+import com.poke_backend.PackPurchaseRequest;
 
 public class SQLHandler {
+	
+	private Connection connection;
+	
+	public SQLHandler() throws SQLException { 
+		
+	}
+	
+//Account/ authentication
+	/*
+	 * creating a new account in the database
+	 */
+    public User createAccount(CreateAccountRequest req) throws SQLException {
+    	
+    	
+    	return null;
+    }
     
+    /**
+     * Attempts to log in an existing user 
+     */
+    public User login(LoginRequest req) throws SQLException { 
+    	
+    	return null;
+    }
+    /**
+     *logs out a user
+     */
+    public void logout(LogoutRequest req) throws SQLException { 
+    	
+    }
+    
+    //USER INVENTORY 
+    /**
+     * Returns the inventory for the given user
+     */
+    public List<UserInventory> getUserInventory(InventoryRequest req) throws SQLException { 
+    	
+    	return null;
+}
+    
+    //Cards/database
+    /**
+     * returns cards from the main database
+     */
+     public List<Card> getCards(AllCardsRequest req) throws SQLException { 
+    	 
+    	 return null;
+     }
+     
+     //Store/Packs
+     /**
+      * Returns available packs 
+      */
+     public List<CardPack> getPacks(PackRequest req) throws SQLException {
+    	 
+    	 return null;
+     }
+     
+     /**
+      * Handles purchasing a pack: 
+      */
+     
+     public List<Card> purchasePack(PackPurchaseRequest req) throws SQLException { 
+    	 
+    	 return null;
+     }
 }
