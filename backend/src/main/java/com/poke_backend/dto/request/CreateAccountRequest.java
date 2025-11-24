ebb357d69b10b04b065db6fd88221e28f4c667de
Alexander Anderson <andersonWork306@gmail.com>
2025-11-23 18:27:05 -0800

    changed create account request in back end

--- backend/src/main/java/com/poke_backend/dto/request/CreateAccountRequest.java
+++ backend/src/main/java/com/poke_backend/dto/request/CreateAccountRequest.java
@@ -3,6 +3,8 @@ package com.poke_backend.dto.request;
 public class CreateAccountRequest {
     public String username;
     public String password;
+    public String email;
+    public String name;
 
     public CreateAccountRequest() {}
 }
