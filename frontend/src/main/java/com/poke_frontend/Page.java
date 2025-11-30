20fe3577957c1c111ea7fc8982d0e9ca78c0d1a1
LucarioWolf <alan.ruelas812@gmail.com>
2025-11-29 23:14:48 -0800

    Fix page

--- frontend/src/main/java/com/poke_frontend/Page.java
+++ frontend/src/main/java/com/poke_frontend/Page.java
@@ -2,22 +2,20 @@ package com.poke_frontend;
 
 public enum Page {
 
-    LOGIN("/fxml/login.fxml", "Login to POKEDOKE", "/css/mainPage.css"),
-    CREATE_ACCOUNT("/fxml/createAccount.fxml", "Create Account!", "/css/mainPage.css"),
-    MAIN_MENU("/fxml/mainMenu.fxml", "POKEDOKE", "/css/mainPage.css"),
-    VIEW_INVENTORY("/fxml/viewCards.fxml", "Inventory", "/css/mainPage.css"),
-    VIEW_DATABASE("/fxml/viewCards.fxml", "The Pokedex", "/css/mainPage.css"),
-    SHOP("/fxml/buyPacks.fxml", "POKEDOKE Store", "/css/mainPage.css"),
+    LOGIN("/fxml/login.fxml", "Login to POKEDOKE"),
+    CREATE_ACCOUNT("/fxml/createAccount.fxml", "Create Account!"),
+    MAIN_MENU("/fxml/mainMenu.fxml", "POKEDOKE"),
+    VIEW_INVENTORY("/fxml/viewCards.fxml", "Inventory"),
+    VIEW_DATABASE("/fxml/viewCards.fxml", "The Pokedex"),
+    SHOP("/fxml/buyPacks.fxml", "POKEDOKE Store"),
     ;
 
     final String fxml;
     final String sceneName;
-    final String styleSheet;
 
-    Page(String fxml, String sceneName, String styleSheet) {
+    Page(String fxml, String sceneName) {
         this.fxml=fxml;
         this.sceneName = sceneName;
-        this.styleSheet = styleSheet;
     }
 
     public String getFXML() {
@@ -26,9 +24,5 @@ public enum Page {
 
     public String getSceneName(){
         return sceneName;
-    }
-
-    public String getCSS(){
-        return styleSheet;
     }
 }
